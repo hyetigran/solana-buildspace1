4f7d0c527d0d01c0c67109b2c4b77efd7fbb5c24
Tigran Asriyan <tigran.asriyan@gmail.com>
2022-02-04 20:44:30 -0500

    remove content

--- programs/buildspace1/src/lib.rs
+++ programs/buildspace1/src/lib.rs
@@ -1,12 +1,1 @@
-use anchor_lang::prelude::*;
 
-#[program]
-pub mod buildspace1 {
-    use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
-        Ok(())
-    }
-}
-
-#[derive(Accounts)]
-pub struct Initialize {}
